bfd513b7500bf9ec8463d5748c85bee0c3079191
sed5qg <sed5qg@virginia.edu>
2018-10-14 19:53:51 -0400

    finished show

--- src/newenv/horseshow/show/models.py
+++ src/newenv/horseshow/show/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Show(models.Model):
-    show_name = models.charField(max_length=100)
-    show_date = models.charField(max_length=100)
-    show_location = models.charField(max_length=100)
+    show_name = models.CharField(max_length=100)
+    show_date = models.CharField(max_length=100)
+    show_location = models.CharField(max_length=100)
     def __str__(self):
         return self.show_name
--- src/newenv/horseshow/show/views.py
+++ src/newenv/horseshow/show/views.py
@@ -1,16 +1,19 @@
 # Create your views here.
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, render
+from django.http import JsonResponse, HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.template import loader
+from django.template.response import TemplateResponse
+from django.urls import resolve, reverse
 import json
-import show.forms
+from show.forms import ShowForm
 from django.forms.models import model_to_dict
 from show.models import Show
 
 
 
 def index(request):
-    latest_show_list = Show.objects.order_by(show_name)
-    template = loader.get_template('show/index.html')
+    latest_show_list = Show.objects.all
+    template = loader.get_template('index.html')
     context = {
         'latest_show_list': latest_show_list,
     }
@@ -18,24 +21,24 @@ def index(request):
 
 
 def create_show(request):
-    form = show.forms.ShowForm()
+    form = ShowForm()
     if request.method == "GET":
         return render(request, 'create_show.html', {'form': form})
-    f = show.forms.ShowForm(request.POST)
+    f = ShowForm(request.POST)
     if not f.is_valid():
         return render(request, 'create_show.html', {'form': f})
     showname = f.cleaned_data['show_name']
     showdate = f.cleaned_data['show_date']
     showlocation = f.cleaned_data['show_location']
-    get_shows = Show.objects.all()
-    all_shows = [show.as_json() for show in get_shows]
-    failure = False
-    for show in all_shows:
-        if showname == show['show_name'] and showdate == show['show_date'] and showlocation == show['show_location']:
-            failure = True
-    if failure:
-        response = {'ok': False, 'error_msg': "This show has already been created!", 'form': form}
-        return render(request, 'create_show.html', response)
+    #get_shows = Show.objects.all()
+    # all_shows = [show for show in get_shows]
+    # failure = False
+    # for show in all_shows:
+    #     if showname == show.object.get(show_name = showname) and showdate == show['show_date'] and showlocation == show['show_location']:
+    #         failure = True
+    # if failure:
+    #     response = {'ok': False, 'error_msg': "This show has already been created!", 'form': form}
+    #     return render(request, 'create_show.html', response)
     new_show = Show.objects.create(show_name=showname, show_date=showdate, show_location=showlocation)
-    response = {'ok': True, 'success_msg': "Show was successfully created", 'form': form, 'show': new_show.as_json()}
+    response = {'ok': True, 'success_msg': "Show was successfully created", 'form': form, 'show': new_show}
     return render(request, 'create_show.html', response)
