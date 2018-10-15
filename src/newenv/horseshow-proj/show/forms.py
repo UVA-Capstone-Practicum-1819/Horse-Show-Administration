64a164c59380e168f8a35de1372763dc11c24eca
lamalla <lm3gc@virginia.edu>
2018-10-14 21:48:32 -0400

    new add horse with horseshowproj

--- src/newenv/horseshow-proj/show/forms.py
+++ src/newenv/horseshow-proj/show/forms.py
@@ -1,9 +1,19 @@
 from django import forms
 from show import views
 from show.models import *
+from .models import *
 
 
 class ShowForm(forms.Form):
     show_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'autocomplete':'off',}))
     show_date = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'autocomplete':'off',}))
     show_location = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'autocomplete':'off',}))
+
+
+
+class Horse(forms.ModelForm):
+
+    class Meta:
+        model = Horse
+        fields = ('name', 'barn_name', 'age','coggins','owner', 'size', 'type')
+
