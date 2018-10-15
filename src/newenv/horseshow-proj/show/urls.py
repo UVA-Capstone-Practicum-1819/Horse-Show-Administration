669ce91372e8ab518cbe3110e4c3045659e2603d
starun96 <tarunsaharya@gmail.com>
2018-10-14 21:58:18 -0400

    changed urls to include login/signup

--- src/newenv/horseshow-proj/show/urls.py
+++ src/newenv/horseshow-proj/show/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from show import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('create_show', views.create_show, name='create_show'),
     path('signup', views.signup, name='signup'),
+    path('login', auth_views.LoginView.as_view(
+        template_name='login.html'), name='login'),
 ]
