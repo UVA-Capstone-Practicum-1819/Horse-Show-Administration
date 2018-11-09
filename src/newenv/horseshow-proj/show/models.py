41a73f24e427c18a87b6d4236c052d9782642a89
annawu030 <yw6fy@virginia.edu>
2018-11-08 22:17:51 -0500

    set primary key for rider to email

--- src/newenv/horseshow-proj/show/models.py
+++ src/newenv/horseshow-proj/show/models.py
@@ -2,18 +2,23 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator, EmailValidator
 import random
 
+
 class Classes(models.Model):
-    class_name = models.CharField(max_length=100, default = "")
-    class_number = models.IntegerField(default = 0)
+    class_name = models.CharField(max_length=100, default="")
+    class_number = models.IntegerField(default=0)
+
     def __str__(self):
         return str(self.class_number) + ". " + self.class_name
 
+
 class Division(models.Model):
-    division_name = models.CharField(max_length=100, default = "")
-    division_number = models.IntegerField(default = 0)
+    division_name = models.CharField(max_length=100, default="")
+    division_number = models.IntegerField(default=0)
     classes = models.ManyToManyField(Classes, blank=True, null=True)
+
     def __str__(self):
         return self.division_name
+
 
 class Show(models.Model):
     show_name = models.CharField(max_length=100)
@@ -41,17 +46,20 @@ class Horse (models.Model):
 class Rider (models.Model):
     name = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
-    age = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(120)])
+    age = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(120)])
     email = models.EmailField(max_length=200, validators=[EmailValidator()])
     horses = models.ManyToManyField(Horse, through='HorseRiderCombo')
 
     def __str__(self):
         return self.name
 
+
 class HorseRiderCombo(models.Model):
     num = models.IntegerField(primary_key=True)
     rider = models.ForeignKey(Rider, on_delete=models.CASCADE)
     horse = models.ForeignKey(Horse, on_delete=models.CASCADE)
+
 
 def random_string():
     rand_str = ""
