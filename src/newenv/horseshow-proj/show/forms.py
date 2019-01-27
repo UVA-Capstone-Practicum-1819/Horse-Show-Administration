e6b837cfb50bda04d23a9996f9f2b9bc4186d27e
free2be <tarun.saharya@protonmail.com>
2019-01-26 22:16:42 -0500

    changed forms to work with models

--- src/newenv/horseshow-proj/show/forms.py
+++ src/newenv/horseshow-proj/show/forms.py
@@ -6,8 +6,8 @@ from django.forms import HiddenInput, formset_factory
 from dal import autocomplete
 
 
-#Form for creating a Show and saving its information. This information doesn't get
-#edited per Bertha and Rebecca's request as the name, date, and location will be set early on
+# Form for creating a Show and saving its information. This information doesn't get
+# edited per Bertha and Rebecca's request as the name, date, and location will be set early on
 class ShowForm(forms.Form):
     name = forms.CharField(max_length=100, widget=forms.TextInput(
         attrs={'autocomplete': 'off', }))
@@ -17,12 +17,16 @@ class ShowForm(forms.Form):
     day_of_price = forms.IntegerField(label="Day-of Price")
     pre_reg_price = forms.IntegerField(label="Preregistration Price")
 
-#This allows you to check whether they were preregistered, or if they entered certain classes the day of
+# This allows you to check whether they were preregistered, or if they entered certain classes the day of
+
+
 class RegistrationBillForm(forms.Form):
     types = ['pre_reg', 'day_of']
     registrationtype = forms.ChoiceField(choices=types)
 
-#This allows you to rank classes from 1st through 6th and store those rankings in the specific Class
+# This allows you to rank classes from 1st through 6th and store those rankings in the specific Class
+
+
 class RankingForm(forms.Form):
     first = forms.IntegerField()
     second = forms.IntegerField()
@@ -37,7 +41,9 @@ class RankingForm(forms.Form):
         model = Class
          """
 
-#This allows you to enter information for an individual Rider. Birth date is necessary for people who are 18 or younger
+# This allows you to enter information for an individual Rider. Birth date is necessary for people who are 18 or younger
+
+
 class RiderForm(forms.ModelForm):
     """ Form for a rider with name, address, email, birth date, whether it is a member of the VHSA, and its county """
     birth_date = forms.DateField(help_text="Only enter if you are 18 or younger", widget=forms.SelectDateWidget(
@@ -48,7 +54,9 @@ class RiderForm(forms.ModelForm):
         fields = ('name', 'address', 'email',
                   'birth_date', 'member_VHSA', 'county',)
 
-#Used to select a rider from the database and prepopulate with options
+# Used to select a rider from the database and prepopulate with options
+
+
 class RiderSelectForm(forms.ModelForm):
     """ Form for selecting a rider and returning its primary key """
     # horses = forms.ModelChoiceField(queryset=Horse.objects.all().order_by('name'))
@@ -63,7 +71,9 @@ class RiderSelectForm(forms.ModelForm):
         model = Rider
         fields = ('rider',)
 
-#This form allows you to enter information about an individual Horse.
+# This form allows you to enter information about an individual Horse.
+
+
 class HorseForm(forms.ModelForm):
     """ the form for a horse, which has a coggins_date, name, accession number, owner, type, and size """
     coggins_date = forms.DateField(widget=forms.SelectDateWidget(
@@ -74,7 +84,9 @@ class HorseForm(forms.ModelForm):
         fields = ('name', 'coggins_date', 'accession_num',
                   'owner', 'type', 'size', )
 
-#This form allows you to select a combo
+# This form allows you to select a combo
+
+
 class ComboSelectForm(forms.ModelForm):
     """ returns the primary key of the horse rider combo in a drop down list """
     # horses = forms.ModelChoiceField(queryset=Horse.objects.all().order_by('name'))
@@ -88,7 +100,9 @@ class ComboSelectForm(forms.ModelForm):
         model = HorseRiderCombo
         fields = ('combo',)
 
-#This combo Num Form is used to give each horse and rider a unique combo number
+# This combo Num Form is used to give each horse and rider a unique combo number
+
+
 class ComboNumForm(forms.Form):
     """ the form for entering a horse rider combo number to go to its edit page """
     # class Meta:
@@ -97,28 +111,36 @@ class ComboNumForm(forms.Form):
     num = forms.IntegerField(
         validators=[MinValueValidator(100), MaxValueValidator(999)])
 
-#This creates the Horse Rider Combo itself
+# This creates the Horse Rider Combo itself
+
+
 class HorseRiderComboCreateForm(forms.ModelForm):
     """ for creating a horse rider combo """
     class Meta:
         model = HorseRiderCombo
         fields = ('num', 'contact', 'email', 'cell')
 
-#This allows you to edit the HorseRiderCombo's infromation. The combo number cannot be changed.
+# This allows you to edit the HorseRiderCombo's infromation. The combo number cannot be changed.
+
+
 class HorseRiderEditForm(forms.ModelForm):
     """ for updating a horse-rider combo """
     class Meta:
         model = HorseRiderCombo
         fields = ('contact', 'email', 'cell')
 
-#This allows you to edit the rider's information
+# This allows you to edit the rider's information
+
+
 class RiderEditForm(forms.ModelForm):
     """ for updating a rider """
     class Meta:
         model = Rider
         fields = ('name', 'address', 'birth_date', 'member_VHSA', 'county')
 
-#This allows you to edit the horse's information
+# This allows you to edit the horse's information
+
+
 class HorseEditForm(forms.ModelForm):
     """ for editing a horse """
     class Meta:
@@ -126,7 +148,9 @@ class HorseEditForm(forms.ModelForm):
         fields = ('accession_num', 'coggins_date',
                   'owner', 'type', 'size')
 
-#This allows you to select a show from a prepopulated list that you can search from
+# This allows you to select a show from a prepopulated list that you can search from
+
+
 class ShowSelectForm(forms.ModelForm):
     # horses = forms.ModelChoiceField(queryset=Horse.objects.all().order_by('name'))
 
@@ -155,7 +179,9 @@ class ShowSelectForm(forms.ModelForm):
         #    )
         #    }
 
-#This allows you to select a horse from a prepopulated list that you can also search from
+# This allows you to select a horse from a prepopulated list that you can also search from
+
+
 class HorseSelectForm(forms.ModelForm):
     horse = forms.ModelChoiceField(
         queryset=Horse.objects.all(),
@@ -166,21 +192,22 @@ class HorseSelectForm(forms.ModelForm):
         model = Horse
         fields = ('horse',)
 
+# This form is a model form for Class.
 
 
-
-#This form is a model form for Class.
 class ClassForm(forms.ModelForm):
     class Meta:
         model = Class
-        fields = ( 'num', )
+        fields = ('num', )
 
 
-#This allows the user to remove classes by entering the class number
+# This allows the user to remove classes by entering the class number
 class RemoveClassForm(forms.Form):
     num = models.IntegerField()
 
-#This form allows the user to select a class from a prepopulated menu
+# This form allows the user to select a class from a prepopulated menu
+
+
 class ClassSelectForm(forms.ModelForm):
     selected_class = forms.ModelChoiceField(
         queryset=Class.objects.all(),
@@ -199,15 +226,19 @@ class ClassSelectForm(forms.ModelForm):
             class_obj.name = class_obj.name
             return class_obj
 
-#This form allows info to be entered for a division
+# This form allows info to be entered for a division
+
+
 class DivisionForm(forms.ModelForm):
-    #create new division
+    # create new division
     class Meta:
         model = Division
         fields = ('name', )
 
-#This form provides information for the division champion of a specific division, recording the
-#champion/champion reserve and their respective points
+# This form provides information for the division champion of a specific division, recording the
+# champion/champion reserve and their respective points
+
+
 class DivisionChampForm(forms.ModelForm):
     class Meta:
         model = Division
@@ -215,7 +246,9 @@ class DivisionChampForm(forms.ModelForm):
         fields = ('champion', 'champion_pts',
                   'champion_reserve', 'champion_reserve_pts')
 
-#This form allows the user to select a division from a prepopulated form
+# This form allows the user to select a division from a prepopulated form
+
+
 class DivisionSelectForm(forms.ModelForm):
     name = forms.ModelChoiceField(
         queryset=Division.objects.all(),
