fe456ad134fa2ba3ee8b69674d79abe31567ac77
starun96 <tarunsaharya@gmail.com>
2019-01-27 16:16:19 -0500

    added some validation to some fields, changed some names to make stuff work

--- src/newenv/horseshow-proj/show/forms.py
+++ src/newenv/horseshow-proj/show/forms.py
@@ -4,11 +4,12 @@ from .models import *
 import datetime
 from django.forms import HiddenInput, formset_factory
 from dal import autocomplete
+from django.core.exceptions import ValidationError
 
 
 class ShowForm(forms.Form):
-    """ 
-    Form for creating a Show and saving its information. This information doesn't get edited per Bertha and Rebecca's request as the name, date, and location will be set early on 
+    """
+    Form for creating a Show and saving its information. This information doesn't get edited per Bertha and Rebecca's request as the name, date, and location will be set early on
     """
     name = forms.CharField(max_length=100, widget=forms.TextInput(
         attrs={'autocomplete': 'off', }))
@@ -20,30 +21,70 @@ class ShowForm(forms.Form):
 
 
 class RegistrationBillForm(forms.Form):
-    """ 
-    This allows you to check whether they were preregistered, or if they entered certain classes the day of 
+    """
+    This allows you to check whether they were preregistered, or if they entered certain classes the day of
     """
     types = ['pre_reg', 'day_of']
     registrationtype = forms.ChoiceField(choices=types)
 
 
 class RankingForm(forms.Form):
-    """ 
-    This allows you to rank classes from 1st through 6th and store those rankings in the specific Class 
     """
-    first = forms.IntegerField()
-    second = forms.IntegerField()
-    third = forms.IntegerField()
-    fourth = forms.IntegerField()
-    fifth = forms.IntegerField()
-    sixth = forms.IntegerField()
+    This allows you to rank classes from 1st through 6th and store those rankings in the specific Class
+    """
+
+    def __init__(self, *args, **kwargs):
+        show_date = kwargs.pop('show_date')
+        super(RankingForm, self).__init__(*args, **kwargs)
+        self.fields['show_date'] = show_date
+
+    def is_valid_combo_num(num):
+        if num < 100 or num > 999:
+            raise ValidationError(
+                _('Number must be between 100 and 999,inclusive'), code="invalid")
+        show = Show.objects.get(date=self.show_date)
+        if show.combos.filter(num=num).count() == 0:
+            raise ValidationError(
+                _('Combination must be in the show'), code="invalid")
+
+    first = forms.IntegerField(
+        validators=[is_valid_combo_num])
+
+    second = forms.IntegerField(
+        validators=[is_valid_combo_num])
+
+    third = forms.IntegerField(
+        validators=[is_valid_combo_num])
+
+    fourth = forms.IntegerField(
+        validators=[is_valid_combo_num])
+
+    fifth = forms.IntegerField(
+        validators=[is_valid_combo_num])
+
+    sixth = forms.IntegerField(
+        validators=[is_valid_combo_num])
 
     fields = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth']
 
+    def clean(self):
+        cleaned_data = super().clean()
+        first = cleaned_data['first']
+        second = cleaned_data['second']
+        third = cleaned_data['third']
+        fourth = cleaned_data['fourth']
+        fifth = cleaned_data['fifth']
+        sixth = cleaned_data['sixth']
+        if first and second and third and fourth and fifth and sixth:
+            field_list = [first, second, third, fourth, fifth, sixth]
+            if len(set(field_list)) != len(field_list):
+                raise ValidationError(
+                    _('Cannot have duplicate combination numbers.'), code="invalid")
+
 
 class RiderForm(forms.ModelForm):
-    """ 
-    This allows you to enter information for an individual Rider. Birth date is necessary for people who are 18 or younger. Form for a rider with name, address, email, birth date, whether it is a member of the VHSA, and its county 
+    """
+    This allows you to enter information for an individual Rider. Birth date is necessary for people who are 18 or younger. Form for a rider with name, address, email, birth date, whether it is a member of the VHSA, and its county
     """
     birth_date = forms.DateField(help_text="Only enter if you are 18 or younger", widget=forms.SelectDateWidget(
         years=range(1900, 2016)))
@@ -55,7 +96,7 @@ class RiderForm(forms.ModelForm):
 
 
 class RiderSelectForm(forms.ModelForm):
-    """ 
+    """
     Form for selecting a rider and returning its primary key. Used to select a rider from the database and prepopulate with options
     """
 
@@ -71,8 +112,8 @@ class RiderSelectForm(forms.ModelForm):
 
 
 class HorseForm(forms.ModelForm):
-    """ 
-    This form allows you to enter information about an individual Horse. The form for a horse, which has a coggins_date, name, accession number, owner, type, and size 
+    """
+    This form allows you to enter information about an individual Horse. The form for a horse, which has a coggins_date, name, accession number, owner, type, and size
     """
     coggins_date = forms.DateField(widget=forms.SelectDateWidget(
         years=range(2010, 2019)))
@@ -97,9 +138,7 @@ class ComboSelectForm(forms.ModelForm):
 
 class ComboNumForm(forms.Form):
     """ # This combo Num Form is used to give each horse and rider a unique combo number. The form for entering a horse rider combo number to go to its edit page """
-    # class Meta:
-    #     model = HorseRiderCombo
-    #     fields = ('num',)
+
     num = forms.IntegerField(
         validators=[MinValueValidator(100), MaxValueValidator(999)])
 
@@ -167,7 +206,7 @@ class ClassForm(forms.ModelForm):
     """ # This form is a model form for Class. """
     class Meta:
         model = Class
-        fields = ('num', )
+        fields = ('num',)
 
 
 class RemoveClassForm(forms.Form):
@@ -177,7 +216,7 @@ class RemoveClassForm(forms.Form):
 
 class ClassSelectForm(forms.ModelForm):
     """ # This form allows the user to select a class from a prepopulated menu """
-    selected_class = forms.ModelChoiceField(
+    class_obj = forms.ModelChoiceField(
         queryset=Class.objects.all(),
         widget=autocomplete.ModelSelect2(
             url='classes_autocomplete'),
@@ -185,7 +224,7 @@ class ClassSelectForm(forms.ModelForm):
 
     class Meta:
         model = Class
-        fields = ('selected_class',)
+        fields = ('num',)
 
     def clean_date(self):
         class_obj = self.cleaned_data['name']
@@ -204,8 +243,8 @@ class DivisionForm(forms.ModelForm):
 
 
 class DivisionChampForm(forms.ModelForm):
-    """ 
-    This form provides information for the division champion of a specific division, recording the champion/champion reserve and their respective points 
+    """
+    This form provides information for the division champion of a specific division, recording the champion/champion reserve and their respective points
     """
     class Meta:
         model = Division
@@ -216,11 +255,11 @@ class DivisionChampForm(forms.ModelForm):
 
 class DivisionSelectForm(forms.ModelForm):
     """ This form allows the user to select a division from a prepopulated form """
-    name = forms.ModelChoiceField(
+    division = forms.ModelChoiceField(
         queryset=Division.objects.all(),
         widget=autocomplete.ModelSelect2(url='division_autocomplete')
     )
 
     class Meta:
         model = Division
-        fields = ('name',)
+        fields = ('division',)
