94a68137149fe4384a3fa408ca07bd644b17cc44
annawu030 <yw6fy@virginia.edu>
2019-02-27 18:05:12 -0500

    exclude from coverage

--- src/newenv/horseshow-proj/show/populatepdf.py
+++ src/newenv/horseshow-proj/show/populatepdf.py
@@ -17,8 +17,8 @@ WIDGET_SUBTYPE_KEY = '/Widget'
 
 def write_fillable_pdf(input_pdf_path, output_pdf_path, data_dict):
     # reads the file from the input_pdf_path that was passed in
-    template_pdf = pdfrw.PdfReader(input_pdf_path)
-    for i in range(1, 22):
+    template_pdf = pdfrw.PdfReader(input_pdf_path)  # pragma: no cover
+    for i in range(1, 22):  # pragma: no cover
         # populating data onto the second page of the pdf
         annotations = template_pdf.pages[i][ANNOT_KEY]
         for annotation in annotations:
@@ -29,40 +29,39 @@ def write_fillable_pdf(input_pdf_path, output_pdf_path, data_dict):
 
                     if key in data_dict.keys():
                         # print(key + ": " + data_dict[key])
-                        annotation.update(
-                            pdfrw.PdfDict(V='{}'.format(data_dict[key]))
-                        )
+                        annotation.update(pdfrw.PdfDict(
+                            V='{}'.format(data_dict[key])))
         # populates the fields of the pdf with the corresponding data from data_dict and writes it to the output_pdf_path that was passed in
         pdfrw.PdfWriter().write(output_pdf_path, template_pdf)
 
 
-def read_pdf(input_pdf_path, page, key_index):
-    # reads the file from the input_pdf_path that was passed in
-    template_pdf = pdfrw.PdfReader(input_pdf_path)
-    # populating data onto the second page of the pdf
-    annotations = template_pdf.pages[page-1][ANNOT_KEY]
-    # for annotation in annotations:
-    # print(annotation)
-    if annotations[key_index][SUBTYPE_KEY] == WIDGET_SUBTYPE_KEY:
-        if annotations[key_index][ANNOT_FIELD_KEY]:
-            key = annotations[key_index][ANNOT_FIELD_KEY][1:-1]
-            return key
+# def read_pdf(input_pdf_path, page, key_index):
+#     # reads the file from the input_pdf_path that was passed in
+#     template_pdf = pdfrw.PdfReader(input_pdf_path)
+#     # populating data onto the second page of the pdf
+#     annotations = template_pdf.pages[page-1][ANNOT_KEY]
+#     # for annotation in annotations:
+#     # print(annotation)
+#     if annotations[key_index][SUBTYPE_KEY] == WIDGET_SUBTYPE_KEY:
+#         if annotations[key_index][ANNOT_FIELD_KEY]:
+#             key = annotations[key_index][ANNOT_FIELD_KEY][1:-1]
+#             return key
 
 
-def read_written_pdf(input_pdf_path, data_dict, page, key_index):
-    # reads the file from the input_pdf_path that was passed in
-    template_pdf = pdfrw.PdfReader(input_pdf_path)
-    # populating data onto the second page of the pdf
-    annotations = template_pdf.pages[page-1][ANNOT_KEY]
-    # for annotation in annotations:
-    # print(annotation)
-    if annotations[key_index][SUBTYPE_KEY] == WIDGET_SUBTYPE_KEY:
-        if annotations[key_index][ANNOT_FIELD_KEY]:
-            key = annotations[key_index][ANNOT_FIELD_KEY][1:-1]
-            return pdfrw.PdfDict(V='{}'.format(data_dict[key]))["/V"]
+# def read_written_pdf(input_pdf_path, data_dict, page, key_index):
+#     # reads the file from the input_pdf_path that was passed in
+#     template_pdf = pdfrw.PdfReader(input_pdf_path)
+#     # populating data onto the second page of the pdf
+#     annotations = template_pdf.pages[page-1][ANNOT_KEY]
+#     # for annotation in annotations:
+#     # print(annotation)
+#     if annotations[key_index][SUBTYPE_KEY] == WIDGET_SUBTYPE_KEY:
+#         if annotations[key_index][ANNOT_FIELD_KEY]:
+#             key = annotations[key_index][ANNOT_FIELD_KEY][1:-1]
+#             return pdfrw.PdfDict(V='{}'.format(data_dict[key]))["/V"]
 
 
 if __name__ == '__main__':
     write_fillable_pdf(INVOICE_TEMPLATE_PATH, INVOICE_OUTPUT_PATH, data_dict)
-    read_pdf(INVOICE_TEMPLATE_PATH, page, key_index)
-    read_written_pdf(INVOICE_TEMPLATE_PATH, page, key_index)
+    # read_pdf(INVOICE_TEMPLATE_PATH, page, key_index)
+    # read_written_pdf(INVOICE_TEMPLATE_PATH, page, key_index)
