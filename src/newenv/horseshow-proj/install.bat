2767426e624b25706fa16571bd16dbe14878ed94
lamalla <lm3gc@virginia.edu>
2019-03-09 23:36:34 -0500

    added success message

--- src/newenv/horseshow-proj/install.bat
+++ src/newenv/horseshow-proj/install.bat
@@ -1,6 +1,7 @@
 IF EXIST "C:Users\Club\Documents\Horse-Show-Administration\src\newenv\horseshow-proj"(
 cd C:Users\Club\Documents\Horse-Show-Administration\src\newenv\horseshow-proj
 git pull
+ECHO "Successfully Updated!"
 pause
 )ELSE(
 ECHO "Directory not found"
