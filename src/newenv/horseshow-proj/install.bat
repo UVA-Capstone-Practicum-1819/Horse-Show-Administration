c67423e2201fa04afece35a6d2fea379ae6c4b82
lamalla <lm3gc@virginia.edu>
2019-03-09 23:31:33 -0500

    fixed correct path names

--- src/newenv/horseshow-proj/install.bat
+++ src/newenv/horseshow-proj/install.bat
@@ -1,23 +1,8 @@
-if not exist "C:Users\Student\Documents\Horse-Show" (
-mkdir C:Users\Student\Documents\Horse-Show
-cd C:Users\Student\Documents\Horse-Show
-git init
-git config --global --unset http.proxy
-git clone https://github.com/UVA-Capstone-Practicum-1819/Horse-Show-Administration.git
-cd C:Users\Student\Documents\Horse-Show
-cd Horse-Show-Administration
-cd src
-cd newenv
-cd horseshow-proj
-python manage.py makemigrations
-python manage.py migrate
-pause
-) ELSE (
-cd C:Users\Student\Documents\Horse-Show
-cd Horse-Show-Administration
-cd src
-cd newenv
-cd horseshow-proj
+IF EXIST "C:Users\Club\Documents\Horse-Show-Administration\src\newenv\horseshow-proj"(
+cd C:Users\Club\Documents\Horse-Show-Administration\src\newenv\horseshow-proj
 git pull
+pause
+)ELSE(
+ECHO "Directory not found"
 pause
 )
